0ecd4cebedc370b2ab798bc98450a444e509e795
nadine2000 <107400308+nadine2000@users.noreply.github.com>
2025-07-15 15:09:56 +0300

    Update frontend.sh

--- frontend.sh
+++ frontend.sh
@@ -15,11 +15,12 @@ terraform plan
 terraform apply -auto-approve
 
 frontend_ip=$(terraform output -raw game_server_public_ip)
+backend_ip=$(terraform output -raw backend_server_private_ip)
 
 
 touch ../ansible/inventory.ini
 echo "[frontend]" > ../ansible/inventory.ini
 echo "$frontend_ip ansible_user=ec2-user ansible_ssh_private_key_file=~/.gkey.pem" >> ../ansible/inventory.ini
 cd ../ansible
-ansible-playbook -i inventory.ini playbook.yml
+ansible-playbook -i inventory.ini playbook.yml --extra-vars "backend_ip=${backend_ip}"
 ssh -i ~/.gkey.pem ec2-user@$frontend_ip
