37e77c14736a1e22430c72bfe380ee8ba449f066
nadine2000 <107400308+nadine2000@users.noreply.github.com>
2025-07-15 14:36:59 +0300

    Create frontend.sh

---
+++ frontend.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+git clone https://github.com/nadine2000/pokemon3
+cd pokemon3/terraform
+
+aws ec2 create-key-pair \
+  --key-name gkey \
+  --key-type rsa \
+  --query 'KeyMaterial' \
+  --output text > ~/.gkey.pem
+
+chmod 400 ~/.gkey.pem
+
+terraform init
+terraform plan
+terraform apply -auto-approve
+
+frontend_ip=$(terraform output -raw game_server_public_ip)
+
+
+touch ../ansible/inventory.ini
+echo "[frontend]" > ../ansible/inventory.ini
+echo "$frontend_ip ansible_user=ec2-user ansible_ssh_private_key_file=~/.gkey.pem" >> ../ansible/inventory.ini
+cd ../ansible
+ansible-playbook -i inventory.ini playbook.yml
+ssh -i ~/.gkey.pem ec2-user@$frontend_ip
