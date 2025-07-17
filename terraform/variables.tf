d60bfd8648ff886e4fee190ac6aed1dfe1e39506
nadine2000 <107400308+nadine2000@users.noreply.github.com>
2025-07-17 23:14:20 +0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,10 +1,3 @@
-
-variable "aws_region" {
-  description = "The AWS region to deploy resources in."
-  type        = string
-  default     = "us-west-2"
-}
-
 variable "backend_server_name" {
   description = "Name for the backend EC2 instance."
   type        = string
@@ -16,17 +9,4 @@ variable "game_server_name" {
   type        = string
   default     = "GameServer"
 }
-
-variable "ami_id" {
-  description = "AMI ID for the EC2 instances."
-  type        = string
-  default     = "ami-05ee755be0cd7555c"
-}
-
-variable "instance_type" {
-  description = "EC2 instance type."
-  type        = string
-  default     = "t3.micro"
-}
-
 
