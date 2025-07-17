0c87e71666849921c9cd004d965e0934c212be0a
nadine2000 <107400308+nadine2000@users.noreply.github.com>
2025-07-17 23:13:56 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,55 +1,24 @@
-resource "aws_instance" "backend_server" {
-  ami           = "ami-05ee755be0cd7555c"
-  instance_type = "t3.micro"
-  subnet_id     = aws_subnet.main_subnet.id
-  vpc_security_group_ids = [aws_security_group.backend_sg.id]
-  associate_public_ip_address = true
-  key_name      = "gkey" 
-  user_data     = file("${path.module}/backend.sh")
-  tags = {
-    Name = "BackendServer"
-  }
-}
-
-resource "aws_instance" "game_server" {
-  ami           = "ami-05ee755be0cd7555c"
-  instance_type = "t3.micro"
-  subnet_id     = aws_subnet.main_subnet.id
-  vpc_security_group_ids = [aws_security_group.game_sg.id]
-  associate_public_ip_address = true
-  key_name      = "gkey"
-  tags = {
-    Name = "GameServer"
-  }
-  provisioner "local-exec" {
-    command = "echo ${self.public_ip} > ../ansible/host_ip.txt"
-  }
-}
-
-
-# network
-
+# ---------- VPC ----------
 resource "aws_vpc" "main_vpc" {
   cidr_block = "10.0.0.0/16"
-
   tags = {
     Name = "MainVPC"
   }
 }
 
+# ---------- Subnet ----------
 resource "aws_subnet" "main_subnet" {
   vpc_id            = aws_vpc.main_vpc.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "us-west-2a"
-
   tags = {
     Name = "MainSubnet"
   }
 }
 
+# ---------- Internet Gateway and Routing ----------
 resource "aws_internet_gateway" "main_igw" {
   vpc_id = aws_vpc.main_vpc.id
-
   tags = {
     Name = "MainIGW"
   }
@@ -57,12 +26,10 @@ resource "aws_internet_gateway" "main_igw" {
 
 resource "aws_route_table" "main_route_table" {
   vpc_id = aws_vpc.main_vpc.id
-
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.main_igw.id
   }
-
   tags = {
     Name = "MainRouteTable"
   }
@@ -73,34 +40,11 @@ resource "aws_route_table_association" "main_route_table_association" {
   route_table_id = aws_route_table.main_route_table.id
 }
 
-# security group
-
+# ---------- Security Groups ----------
 resource "aws_security_group" "backend_sg" {
   name        = "backend_security_group"
-  description = "Allow SSH, Flask API, and MongoDB traffic"
+  description = "Allow SSH, Flask, MongoDB"
   vpc_id      = aws_vpc.main_vpc.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 5000
-    to_port     = 5000
-    protocol    = "tcp"
-    cidr_blocks = [aws_vpc.main_vpc.cidr_block]
-    self        = true
-  }
-
-  ingress {
-    from_port   = 27017
-    to_port     = 27017
-    protocol    = "tcp"
-    cidr_blocks = ["172.17.0.0/16"] # Docker 
-  }
 
   egress {
     from_port   = 0
@@ -116,16 +60,8 @@ resource "aws_security_group" "backend_sg" {
 
 resource "aws_security_group" "game_sg" {
   name        = "game_security_group"
-  description = "Allow SSH and all outbound traffic"
+  description = "Allow SSH and all outbound"
   vpc_id      = aws_vpc.main_vpc.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    self        = true
-  }
 
   egress {
     from_port   = 0
@@ -139,4 +75,62 @@ resource "aws_security_group" "game_sg" {
   }
 }
 
+# ---------- Security Group Rules ----------
 
+# Allow SSH to Backend
+resource "aws_security_group_rule" "backend_allow_ssh" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.backend_sg.id
+}
+
+# Allow Flask port 5000 from Game SG
+resource "aws_security_group_rule" "backend_allow_flask_from_game" {
+  type                     = "ingress"
+  from_port                = 5000
+  to_port                  = 5000
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.game_sg.id
+  security_group_id        = aws_security_group.backend_sg.id
+}
+
+# Allow SSH to Game EC2
+resource "aws_security_group_rule" "game_allow_ssh" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.game_sg.id
+}
+
+# ---------- EC2 Instances ----------
+resource "aws_instance" "backend_server" {
+  ami                         = "ami-05ee755be0cd7555c"
+  instance_type               = "t3.micro"
+  subnet_id                   = aws_subnet.main_subnet.id
+  vpc_security_group_ids      = [aws_security_group.backend_sg.id]
+  associate_public_ip_address = true
+  key_name                    = "gkey"
+  user_data                   = file("${path.module}/backend.sh")
+
+  tags = {
+    Name = "BackendServer"
+  }
+}
+
+resource "aws_instance" "game_server" {
+  ami                         = "ami-05ee755be0cd7555c"
+  instance_type               = "t3.micro"
+  subnet_id                   = aws_subnet.main_subnet.id
+  vpc_security_group_ids      = [aws_security_group.game_sg.id]
+  associate_public_ip_address = true
+  key_name                    = "gkey"
+
+  tags = {
+    Name = "GameServer"
+  }
+}
